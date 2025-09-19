8f71fb08b91a5b0c357c6b5f80a9788d86297253
2notprime <long3172004@gmail.com>
2025-09-19 01:43:02 -0700

    .

--- src/service/celery_app.py
+++ src/service/celery_app.py
@@ -1,9 +1,16 @@
 from celery import Celery
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+broker_url = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
+backend_url = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
 
 celery = Celery(
     "tasks",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0",
+    broker=broker_url,
+    backend=backend_url,
     include=["service.import_tasks"]
 )
 
